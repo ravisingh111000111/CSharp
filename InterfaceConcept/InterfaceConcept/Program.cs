42d714156e4953e6dece2f744f7b41ac0ec2e49e
ravisingh111000111 <54453224+ravisingh111000111@users.noreply.github.com>
2022-08-20 16:20:54 +0530

    Add files via upload

---
+++ InterfaceConcept/InterfaceConcept/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace InterfaceConcept
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            ChineseChef chefXi = new ChineseChef();
+            KoreanChef chefKimJong = new KoreanChef();
+
+            /**If chef Kim Jong is on leave then waiter can pass the order to chef chef Xi as he also knows to prepare noodle.
+             * Here we have achieved loose coupliing
+             * **/
+
+            Waiter waiter = new Waiter();
+            waiter.instructChefToCook(chefXi);
+        }
+    }
+
+    interface IChef
+    {
+        void makeNoodles();
+    }
+
+
+    class ChineseChef : IChef
+    {
+        public void makeNoodles()
+        {
+            Console.WriteLine("Noodles ready!!");
+        }
+    }
+
+    class KoreanChef : IChef
+    {
+        public void makeNoodles()
+        {
+            Console.WriteLine("Noodles ready!!");
+
+        }
+    }
+
+    class Waiter
+    {
+        public void instructChefToCook(IChef chef)
+        {
+            Console.WriteLine("Instruction passed to chef. Food is getting ready!");
+            chef.makeNoodles();
+        }
+    }
+
+
+}
